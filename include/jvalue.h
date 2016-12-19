6cbc0b12c86ed0ad8849f23de906bbab9242a273
Ilia Vinokurov <ilvin@mail.ru>
2016-12-19 14:48:17 +0300

    Added forgotten files

--- include/jvalue.h
+++ include/jvalue.h
@@ -8,25 +8,184 @@
 namespace NJValue {
 
     class IJValue;
+
+    enum EJValueType { JUNDEFINED, JNULL, JBOOL, JINTEGER, JDOUBLE, JSTRING, JARRAY, JMAP };
+
+    class JSON_UNDEFINED;
+    class JSON_NULL;
+    class JSON_INTEGER;
+//    class JSON_DOUBLE;
+    class JSON_STRING;
+//    class JSON_BOOL;
+    class JSON_ARRAY;
+//    class JSON_MAP;
+
+    class JSON_UNDEFINED {
+        public:
+        EJValueType GetType() const;
+
+        operator std::string() const { return ""; }
+        operator bool() const { return false; }
+        operator long() const { return 0; }
+        operator double() const { return 0.0; }
+    };
+
+    class JSON_NULL {
+        public:
+
+        EJValueType GetType() const;
+
+        operator std::string() const { return ""; }
+        operator bool() const { return false; }
+        operator long() const { return 0; }
+        operator double() const { return 0.0; }
+    };
+
+    class JSON_STRING {
+        std::string value;
+        public:
+
+        JSON_STRING(std::string value = "") : value(value) { }
+        JSON_STRING(const JSON_STRING& val) : value(val.value) { }
+
+        EJValueType GetType() const;
+
+        operator std::string() const { return value; }
+        operator bool() const {
+            return (value == "" || value == "null" || value == "0" || value == "false")
+                ? false
+                : true;
+        }
+        operator long() const {
+            try {
+                return (value == "" || value == "null" || value =="false")
+                    ? 0
+                    : ( value == "true" )
+                        ? 1
+                        : std::stol(value);
+            } catch (std::invalid_argument) {
+                return 0;
+            }
+        }
+        operator double() const {
+            try {
+                return (value == "" || value == "null" || value =="false")
+                    ? 0.0
+                    : ( value =="true" )
+                        ? 1.0
+                        : std::stod(value);
+            } catch (std::invalid_argument) {
+                return 0.0;
+            }
+        }
+    };
+
+    class JSON_INTEGER {
+        long value;
+        public:
+
+        JSON_INTEGER(long value = 0) : value(value) { }
+        JSON_INTEGER(const JSON_INTEGER& val) : value(val.value) { }
+
+        EJValueType GetType() const;
+
+        operator std::string() const { return std::to_string(value); }
+        operator bool() const { return value == 0 ? false : true; }
+        operator long() const { return value; }
+        operator double() const { return (double)value; }
+    };
+
+    class JSON_ARRAY : public std::deque<IJValue> {
+        public:
+
+        EJValueType GetType() const;
+
+        operator std::string() const { return "ARRAY"; }
+        operator bool() const { return size() == 0 ? false : true; }
+        operator long() const { return size(); }
+        operator double() const { return (double)size(); }
+    };
+
+    class IJValue {
+    public:
+        virtual bool IsUndefined() const { return GetType() == JUNDEFINED; }
+        virtual bool IsNull() const { return GetType() == JNULL; }
+        virtual bool IsBool() const { return GetType() == JBOOL; }
+        virtual bool IsInteger() const { return GetType() == JINTEGER; }
+        virtual bool IsDouble() const { return GetType() == JDOUBLE; }
+        virtual bool IsString() const { return GetType() == JSTRING; }
+        virtual bool IsArray() const { return GetType() == JARRAY; }
+        virtual bool IsMap() const { return GetType() == JMAP; }
+
+        virtual EJValueType GetType() const { return JUNDEFINED; }
+        virtual std::string AsString() const { return ""; };
+        virtual bool AsBool() const { return false; };
+        virtual long AsInteger() const { return 0; };
+        virtual double AsDouble() const { return 0; };
+
+        virtual JSON_ARRAY ToArray() const {
+            JSON_ARRAY a;
+            a.push_back(*this);
+            return a;
+        }
+
+        virtual ~IJValue() {
+        }
+    };
+
     template<class T>
-    class TJValue;
+    class TJValue : public IJValue {
+        T val;
 
-    enum EJValueType { UNKNOWN, UNDEFINED, BOOL, INTEGER, DOUBLE, STRING, ARRAY, MAP };
+    public:
 
-    struct TUnknown {};
-    struct TUndefined {};
+        TJValue() {
+        }
 
-    using JSON_UNKNOWN = TUnknown;
-    using JSON_UNDEFINED = TUndefined;
-    using JSON_INTEGER = int;
-    using JSON_DOUBLE = double;
-    using JSON_STRING = std::string;
-    using JSON_BOOL = bool;
-    using JSON_ARRAY = std::deque<IJValue>;
-    using JSON_MAP = std::map<std::string, IJValue>;
+        TJValue(const T& value) : val(value) {
+        }
 
+        TJValue(const int& value) : val(T(JSON_INTEGER(value))) {
+        }
 
+//        TJValue(const double& value) : val(T(JSON_DOUBLE(value))) {
+//        }
 
+        TJValue(const std::string& value) : val(T(JSON_STRING(value))) {
+        }
+
+        TJValue(const char* value) : val(T(JSON_STRING(std::string(value)))) {
+        }
+
+        virtual ~TJValue() {
+        }
+
+        virtual EJValueType GetType() const {
+            return val.GetType();
+        }
+
+        virtual const T& GetValue() const {
+            return val;
+        }
+
+        virtual std::string AsString() const {
+            return (std::string)val;
+        }
+
+        virtual bool AsBool() const {
+            return (bool)val;
+        }
+
+        virtual long AsInteger() const {
+            return (long)val;
+        }
+
+        virtual double AsDouble() const {
+            return (double)val;
+        }
+
+    };
+/*
     class IJValue {
     protected:
 
@@ -156,7 +315,7 @@ namespace NJValue {
         }
 
     };
-
+*/
 /*
     using TJValuePtr = TJValue*;
     using TJArrayContainer = std::vector<TJValuePtr>;
--- main.cpp
+++ main.cpp
@@ -7,6 +7,25 @@ using namespace std;
 
 int main() {
 
+    JSON_INTEGER ji1;
+    TJValue<JSON_INTEGER> ji2 = "5";
+    ji2 = 13;
+    TJValue<JSON_STRING> js1 = "Hello world!!!";
+
+    TJValue<JSON_STRING> js2 = js1;
+
+    IJValue * ju1 = &ji2;
+
+    std::cout << "ji2 = " << ji2.AsString() << "\n";
+    std::cout << "js1 = " << js1.AsString() << "\n";
+    std::cout << "js2 = " << js2.AsString() << "\n";
+    std::cout << "ju1 = " << ju1->AsString() << "\n";
+
+
+
+//    std::cout << "type1 = " << ju1->GetType() << "; type2 = " << ji2.GetType() << "\n";
+
+/*
     TJValue<JSON_INTEGER> a = 0;
     TJValue<JSON_DOUBLE> b = (JSON_DOUBLE)7.17;
     TJValue<JSON_STRING> c = (JSON_STRING)"";
@@ -22,6 +41,6 @@ int main() {
     cout << "b is " << (JSON_BOOL)b << endl;
     cout << "c is " << c.ToBool() << endl;
     cout << "d is " << d.ToBool() << endl;
-
+*/
     return 0;
 }
